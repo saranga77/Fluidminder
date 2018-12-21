35873212393e120fba08bd9cec6cf2e7340cafc5
saranga geeganage <sarangadinusha@gmail.com>
2018-12-21 09:02:14 +0530

    images added

--- Fluidminder/ViewController.swift
+++ Fluidminder/ViewController.swift
@@ -19,9 +19,7 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    override func viewDidAppear(_ animated: Bool) {
-        //
-    }
+    
 
 }
 
