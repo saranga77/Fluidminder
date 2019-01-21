1109d394efffa16814818d494ae043d4cda31e96
saranga geeganage <sarangageeganage@sarangas-macbook-pro.local>
2019-01-21 14:28:11 +0530

    2nd viewcontroller added

--- Fluidminder/ViewController.swift
+++ Fluidminder/ViewController.swift
@@ -13,22 +13,16 @@ class ViewController: UIViewController {
     @IBOutlet var manView: BAFluidView!
     let mask = UIImageView(image:#imageLiteral(resourceName: "BOTTELS_0005_2000px-Human_outline"))
     
-
+    @IBOutlet var urineValue: UISlider!
+    
+    var totalFluidVolume = 0
+    var dehydrationIndex = 0.0
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        mask.frame = manView.bounds
-        mask.contentMode = .scaleAspectFit
-        
-        manView.mask = mask
-        
-        
-        manView.alpha = 0.9
-        manView.fillColor = UIColor.blue
-        manView.keepStationary()
-        manView.fill(to: 0.6)
-        manView.amplitudeIncrement = 100
+        fluidRun()
+       
     
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -38,6 +32,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    func fluidRun (){
+        mask.contentMode = .scaleAspectFit
+        mask.frame = manView.bounds
+        manView.mask = mask
+        manView.alpha = 0.9
+        manView.fillColor = UIColor.blue
+        manView.keepStationary()
+        manView.fill(to: 0.6)
+        manView.amplitudeIncrement = 100
+    }
 }
 
